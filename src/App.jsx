6e1296f5b83ea20019d135a08ec263134f3744f5
Sandra Sika <shika4days@gmail.com>
2024-03-29 15:23:35 +0800

    feat: i react-router-dom &  add route to each page

--- src/App.jsx
+++ src/App.jsx
@@ -6,18 +6,25 @@ import PalettePage from "../src/pages/myPalette";
 import CraftHubPage from "../src/pages/myCraftHub";
 import GalleryPage from "../src/pages/myGallery";
 
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+
+
 
 function App() {
   return (
     <>
-      <h1>hello, Sandra</h1>
-      < DemoPage />
-      < LoginPage />
-      < RegisterPage />
-      < UserPage />
-      < PalettePage />
-      < CraftHubPage />
-      < GalleryPage />
+    <BrowserRouter>
+      <Routes>
+       <Route path="*" element={<DemoPage />}></Route>
+       <Route path="login" element={<LoginPage />}></Route>
+       <Route path="register" element={<RegisterPage />}></Route>
+       <Route path="user" element={<UserPage />}></Route>
+       <Route path="palette" element={<PalettePage />}></Route>
+       <Route path="crafthub" element={<CraftHubPage />}></Route>
+       <Route path="gallery" element={<GalleryPage />}></Route>
+      </Routes>
+    </BrowserRouter>
     </>
   )
 }
