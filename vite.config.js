733f18d373185979b5c8037ec8d70cbba6378ae5
Sandra Sika <shika4days@gmail.com>
2024-04-22 16:19:16 +0800

    Prepare for deployment

--- vite.config.js
+++ vite.config.js
@@ -4,8 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "/Your-Own-Crochet-Palette/",
   server: {
-    host: '0.0.0.0', // 将服务器绑定到所有网络接口，以便容器外部可以访问
-    port: 5173 // 指定服务器监听的端口
+    host: '0.0.0.0', 
+    port: 5173 
   }
 })
